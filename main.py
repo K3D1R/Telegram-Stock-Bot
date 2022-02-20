6aa1a4b221eaf43d91674b3e8fd7e255b9ec80c1
K3D1R <84622375+K3D1R@users.noreply.github.com>
2022-02-20 20:12:06 +0400

    Main

---
+++ main.py
@@ -0,0 +1,45 @@
+import logging
+from parsr import price, update_ticker
+import emoji
+
+from aiogram import Bot, Dispatcher, executor, types
+from aiogram.types import ReplyKeyboardRemove, \
+    ReplyKeyboardMarkup, KeyboardButton, \
+    InlineKeyboardMarkup, InlineKeyboardButton
+#bot's token
+API_TOKEN = '5166586300:AAEExuhGbEUQdam4AHnIXaay7BgYwzcruh8'
+
+# Configure logging
+logging.basicConfig(level=logging.INFO)
+
+# Initialize bot and dispatcher
+bot = Bot(token=API_TOKEN)
+dp = Dispatcher(bot)
+
+button_help = KeyboardButton('/FAQ')
+button_price = KeyboardButton('/Цена_акции')
+
+greet_kb = ReplyKeyboardMarkup(resize_keyboard=True)
+greet_kb.add(button_help, button_price)
+
+@dp.message_handler(commands = ['start'])
+async def send_welcome(message: types.Message):
+    await message.reply("Привет \n Я stok_bot. Я помогу тебе узнать стоимость акции SBERP", reply_markup= greet_kb)
+
+@dp.message_handler(commands = ['FAQ'])
+async def help(message: types.Message):
+    FAQ =("FAQ\n  К сожалению сейчас отслеживаются только привилегированные акции Cбербанка(SBERP)"
+    "\n  Бот работает не совсем стабильно"
+    "\n  Цены акций берутся с сайта РБК:\n https://quote.rbc.ru/ticker/59763 ")
+    await message.reply(FAQ)
+
+@dp.message_handler(commands = ['Цена_акции'])
+async def getprice(message: types.Message):
+    await message.reply(f"Цена акций SBERP:{price}₽\nSBERP:https://quote.rbc.ru/ticker/59763")
+
+
+if __name__ == '__main__':
+     executor.start_polling(dp, skip_updates=True)
+
+
+    

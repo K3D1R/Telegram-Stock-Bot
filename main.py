543d552497be171afb9ad2a531484fd423643b90
K3D1R <84622375+K3D1R@users.noreply.github.com>
2022-04-06 21:50:49 +0400

    main.py
    New:
    Wide menu
    Quick tickers button
    Quick currency menu

--- main.py
+++ main.py
@@ -1,48 +1,54 @@
+#Импорт всех нужных библиотек
+from audioop import add
 import logging
-import emoji
-
 from turtle import update
 from aiohttp import request
-import requests #Для осуществления URL запросов
-from bs4 import BeautifulSoup # импорт библиотеки для работы с html
-import time # импорт библиотеки для установки задержки
+from matplotlib import ticker
+import requests  #Для осуществления URL запросов
+from bs4 import BeautifulSoup  #импорт библиотеки для работы с html
+
 
 from aiogram import Bot, Dispatcher, executor, types
-from aiogram.types import ReplyKeyboardRemove, \
-    ReplyKeyboardMarkup, KeyboardButton, \
-    InlineKeyboardMarkup, InlineKeyboardButton
+from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
+from aiogram.dispatcher.filters.state import StatesGroup, State
+from aiogram.dispatcher import FSMContext
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
+from aiogram.contrib.middlewares.logging import LoggingMiddleware
+
+class tickker(StatesGroup):
+    tickr_cost = State()
 #bot's token
-API_TOKEN = '5166586300:AAEExuhGbEUQdam4AHnIXaay7BgYwzcruh8'
+API_TOKEN = ''
 
 # Configure logging
 logging.basicConfig(level=logging.INFO)
 
 # Initialize bot and dispatcher
 bot = Bot(token=API_TOKEN)
-dp = Dispatcher(bot)
-
-button_help = KeyboardButton('/FAQ')
-button_price = KeyboardButton('/Цена_акции')
-
+dp = Dispatcher(bot, storage=MemoryStorage())
+dp.middleware.setup(LoggingMiddleware())
+#Меню
+button_currnecy = KeyboardButton('/💹Валюта')
+#Валюта
+button_rubble_dollar_usa = KeyboardButton('/Доллар_США_＄')
+button_rubble_euro = KeyboardButton('/€_Евро')
+button_rubble_yan = KeyboardButton('/¥_Юань')
+back_button = KeyboardButton('/◀Назад')
+#Мини меню
+button_help = KeyboardButton('/🤔FAQ')
+button_price = KeyboardButton('/Цена_акции🧾')
+#Быстрые акции
+button_GAZP = KeyboardButton('/🔥GAZP')
+button_SBER = KeyboardButton('/🏦SBER')
+button_ROSN = KeyboardButton('/🛢ROSN')
+# клавиатуры
+currency_kb = ReplyKeyboardMarkup(resize_keyboard=True)
+currency_kb.add(button_rubble_dollar_usa, button_rubble_euro, button_rubble_yan, back_button)
 greet_kb = ReplyKeyboardMarkup(resize_keyboard=True)
-greet_kb.add(button_help, button_price)
+greet_kb.add(button_currnecy, button_help, button_price).add(button_ROSN, button_GAZP, button_SBER)
 
-@dp.message_handler(commands = ['start'])
-async def send_welcome(message: types.Message):
-    await message.reply("Привет \n Я stok_bot. Я помогу тебе узнать стоимость акции SBERP", reply_markup= greet_kb)
-
-@dp.message_handler(commands = ['FAQ'])
-async def help(message: types.Message):
-    FAQ =("FAQ\n  К сожалению сейчас отслеживаются только привилегированные акции Cбербанка(SBERP)"
-    "\n  Бот работает не совсем стабильно"
-    "\n  Цены акций берутся с сайта РБК:\n https://quote.rbc.ru/ticker/59763 ")
-    await message.reply(FAQ)
-
-@dp.message_handler(commands = ['Цена_акции'])
-async def getprice(message: types.Message):
-    def price_ticker():
-        #Ссылка на актив
-        SBERP = "https://quote.rbc.ru/ticker/59763"
+#Функия для выдачи цены.
+def price_ticker(url):
 
         #заголовок для URL запроса, для избежания распозновия Бот-запроса
         headers = {
@@ -50,25 +56,88 @@ async def getprice(message: types.Message):
         }
 
         #переменная, для соххранения html разметки страницы
-        html = requests.get(SBERP, headers)
+        html = requests.get(url, headers)
 
         #парсим теги в переменную soup
-        soup =BeautifulSoup(html.content, 'html.parser')
+        soup = BeautifulSoup(html.content, 'html.parser')
         #поиск цены акции
         convert = soup.find('span',{'class':"chart__info__sum"})
         #формирование выдачи
-        price = float(convert.text[1:].strip().replace(',','.'))
+        price = float(convert.text[1:].strip().replace(',','.').replace(' ',''))#можно сделать короче, но на всякий случай пусть будет так
         currency = convert.text[:1]
-        stock_price = str(price)+currency
-        print(f'Цена акции SBERP:{stock_price}')
-    
-        return stock_price
-    price = price_ticker()
-    await message.reply(f"Цена акций SBERP:{price}\nSBERP:https://quote.rbc.ru/ticker/59763")
+        currency_price = str(price)+currency
+        print(f'Цена евро:{currency_price}')
+        #таймер
+        return currency_price
 
+#Обработчики
+@dp.message_handler(commands = ['start'])
+async def send_welcome(message: types.Message):
+    await message.bot.send_message(message.from_user.id, "Привет \n Я stoсk_bot. Я помогу тебе узнать стоимость акций различных компаний", reply_markup= greet_kb)
+    
+@dp.message_handler(commands = ['🤔FAQ'])
+async def help(message: types.Message):
+    FAQ =(f"FAQ\n {u'⚠'} К сожалению поддерживаются только ссылки с сайта РБК"
+    "\n ⚠ Бот работает не совсем стабильно"
+    "\n ⚠ Цены акций берутся с сайта РБК:\n https://quote.rbc.ru/")
+    await message.reply(FAQ)
+
+#реакция на команду выдача цены актива
+@dp.message_handler(commands = ['Цена_акции🧾'], state = None)
+async def getURL_part1(message: types.Message):
+    await message.reply("Вставь ссылку на акцию с сайта РБК")
+    await tickker.tickr_cost.set()
+
+@dp.message_handler(state = tickker.tickr_cost)
+async def ticker_url(message:types.Message, state:FSMContext):
+    url = message.text
+    sprice = price_ticker(url)
+    await message.reply(f"💹Цена акции:{sprice}🧾\n :{url}")
+    await state.finish()
+
+@dp.message_handler(commands= ['💹Валюта'])
+async def currency_choose(message:types.Message):
+    await message.reply("Выберите валюту, нажав на клавиатуру.", reply_markup=currency_kb)
+@dp.message_handler(commands=['Доллар_США_＄'])
+async def get_currency(message:types.Message):
+    url = 'https://quote.rbc.ru/ticker/59111'
+    sprice = price_ticker(url)
+    await message.reply(f"＄ Цена доллара:{sprice}\n :{url}")
+
+@dp.message_handler(commands=['€_Евро'])
+async def get_currency(message:types.Message):
+    url = 'https://quote.rbc.ru/ticker/59090'
+    sprice = price_ticker(url)
+    await message.reply(f"€ Цена евро:{sprice}\n :{url}")
+
+@dp.message_handler(commands=['¥_Юань'])
+async def get_currency(message:types.Message):
+    url = 'https://quote.rbc.ru/ticker/59066'
+    sprice = price_ticker(url)
+    await message.reply(f"¥ Цена юаня:{sprice}\n :{url}")
+
+@dp.message_handler(commands=['🔥GAZP'])
+async def get_currency(message:types.Message):
+    url = 'https://quote.rbc.ru/ticker/59256'
+    sprice = price_ticker(url)
+    await message.reply(f"💹Цена Газпрома: {sprice}🧾\n :{url}")
+
+@dp.message_handler(commands=['🛢ROSN'])
+async def get_currency(message:types.Message):
+    url = 'https://quote.rbc.ru/ticker/59430'
+    sprice = price_ticker(url)
+    await message.reply(f"🛢Цена Роснефти:{sprice}🧾\n :{url}")
+
+@dp.message_handler(commands=['🏦SBER'])
+async def get_currency(message:types.Message):
+    url = 'https://quote.rbc.ru/ticker/59762'
+    sprice = price_ticker(url)
+    await message.reply(f"🏦Цена Сбербанка: {sprice}🧾\n :{url}")
+
+
+@dp.message_handler(commands= ['◀Назад'])
+async def back(message:types.Message):
+    await message.reply("Выберите операцию", reply_markup=greet_kb)
 
 if __name__ == '__main__':
      executor.start_polling(dp, skip_updates=True)
-
-
-    
